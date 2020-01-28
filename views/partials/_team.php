3f11833e00f378083f6186ca3d43ed89ba6e9402
Erick <erickluques@gmail.com>
2020-01-28 02:04:52 -0200

    Create _team.php

---
+++ views/partials/_team.php
@@ -0,0 +1,77 @@
+<!-- TEAM / BARBERS -->
+	<section id="team-barber" class="team-barber">
+		<div class="container">
+			<div class="h-page-title">
+				<h1>the barbers</h1>
+				<span class="sub-title">our team</span>
+				<div class="mustache">
+					<span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+				</div>
+			</div>
+		</div>
+		<div class="main-team">
+			<ul class="grid cs-style-7">
+				<li>
+					<img class="img-responsive" src="assets/images/home-page/team-img1.png" alt=""/>
+					<div class="figcaption">
+						<h5>Leon Holm</h5>
+						<p>Mauris risus dolor, pretium quis placerat vitae, suscipit at nisi. Duis consectetur euismod </p>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<img class="img-responsive" src="assets/images/home-page/team-img2.png" alt=""/>
+					<div class="figcaption">
+						<h5>Leon Holm</h5>
+						<p>Mauris risus dolor, pretium quis placerat vitae, suscipit at nisi. Duis consectetur euismod </p>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<img class="img-responsive" src="assets/images/home-page/team-img3.png" alt=""/>
+					<div class="figcaption">
+						<h5>Leon Holm</h5>
+						<p>Mauris risus dolor, pretium quis placerat vitae, suscipit at nisi. Duis consectetur euismod </p>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<img class="img-responsive" src="assets/images/home-page/team-img4.png" alt=""/>
+					<div class="figcaption">
+						<h5>Leon Holm</h5>
+						<p>Mauris risus dolor, pretium quis placerat vitae, suscipit at nisi. Duis consectetur euismod </p>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<img class="img-responsive" src="assets/images/home-page/team-img5.png" alt=""/>
+					<div class="figcaption">
+						<h5>Leon Holm</h5>
+						<p>Mauris risus dolor, pretium quis placerat vitae, suscipit at nisi. Duis consectetur euismod </p>
+						<ul class="social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<!-- TEAM / BARBERS -->

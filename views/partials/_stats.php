9dd37c1fa78cb4c49bee18b5ff04b78112ca1d8c
Erick <erickluques@gmail.com>
2020-01-28 02:04:44 -0200

    Create _stats.php

---
+++ views/partials/_stats.php
@@ -0,0 +1,28 @@
+<!-- STATS -->
+<section id="stats">
+    <div class="stats">
+        <div class="container">
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <h1>1500</h1>
+                <img src="assets/images/mustache.png" alt="">
+                <h6>Clients</h6>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <h1>490</h1>
+                <img src="assets/images/mustache.png" alt="">
+                <h6>CUPS OF COFFEE</h6>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <h1>2708</h1>
+                <img src="assets/images/mustache.png" alt="">
+                <h6>likes</h6>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3">
+                <h1>1500</h1>
+                <img src="assets/images/mustache.png" alt="">
+                <h6>best looks</h6>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- STATS -->

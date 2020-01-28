c59be9014ec66079644da2af0fac92f13f230df8
Erick <erickluques@gmail.com>
2020-01-28 02:04:31 -0200

    Create _pricing.php

---
+++ views/partials/_pricing.php
@@ -0,0 +1,49 @@
+<!-- PRICING -->
+<section id="pricing" class="pricing">
+    <div class="pricing-bg">
+        <div class="container">
+            <div class="h-page-title">
+                <h1>Pricing</h1>
+                <span class="sub-title">hot deals</span>
+                <div class="mustache">
+                    <span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+                </div>
+            </div>
+            <div class="main-pricing">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                        <div class="price-block">
+                            <div class="inner-price">
+                                <h3>traditional haircuts</h3>
+                                <h1>23<span>$</span></h1>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its</p>
+                                <button class="main-btn" type="submit">Order</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                        <div class="price-block">
+                            <div class="inner-price">
+                                <h3>Shaves</h3>
+                                <h1>12<span>$</span></h1>
+                                <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</p>
+                                <button class="main-btn" type="submit">Order</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                        <div class="price-block">
+                            <div class="inner-price">
+                                <h3>stubble</h3>
+                                <h1>35<span>$</span></h1>
+                                <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text</p>
+                                <button class="main-btn" type="submit">Order</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- PRICING -->

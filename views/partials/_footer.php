36f366572a77f3631123b9abfac29c6f470a134b
Erick <erickluques@gmail.com>
2020-01-28 13:21:59 -0200

    Update _footer.php

--- views/partials/_footer.php
+++ views/partials/_footer.php
@@ -1,20 +1,21 @@
 <!-- FOOTER -->
 <footer id="footer" class="blog-f">
     <div class="container">
-        <span>Follow, Like, Pin all the social stuff</span>
+        <span>Siga-nos</span>
         <ul class="footer-social">
-            <li><a href="#" class="fa fa-twitter"></a></li>
+            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
             <li><a href="#" class="fa fa-facebook"></a></li>
-            <li><a href="#" class="fa fa-google-plus"></a></li>
+            <li><a href="#" class="fa fa-instagram"></a></li>
+            <!-- <li><a href="#" class="fa fa-google-plus"></a></li>
             <li><a href="#" class="fa fa-dribbble"></a></li>
             <li><a href="#" class="fa fa-linkedin"></a></li>
             <li><a href="#" class="fa fa-pinterest"></a></li>
             <li><a href="#" class="fa fa-flickr"></a></li>
             <li><a href="#" class="fa fa-youtube"></a></li>
-            <li><a href="#" class="fa fa-rss"></a></li>
+            <li><a href="#" class="fa fa-rss"></a></li> -->
         </ul>
-        <p>Barbershop &copy;Copyright 2015</p>
-        <p>All rights reserved <a target="_blank" href="http://themeforest.net/user/Theme-Squared">ThemeSquared</a></p>
+        <p>&copy; 2020 Barbearia Progresso</p>
+        <p>Desenvolvido por <a target="_blank" href="#">Érick Firmo.</a> Todos os direitos reservados.</p>
     </div>
 </footer>
 <!-- FOOTER -->

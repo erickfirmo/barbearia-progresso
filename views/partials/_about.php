e25fdac1a45baca2269fba715cefbb3120c25963
Erick <erickluques@gmail.com>
2020-01-28 02:03:30 -0200

    Create _about.php

---
+++ views/partials/_about.php
@@ -0,0 +1,35 @@
+<!-- ABOUT -->
+<section id="about" class="about">
+    <div class="container">
+
+        <div class="h-page-title">
+            <h1>Sobre</h1>
+            <span class="sub-title">Um pouco sobre nós</span>
+            <div class="mustache">
+                <span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+            </div>
+        </div>
+
+        <div class="about-main">
+            <div class="row">
+                <div class="class-xs-12 col-sm-6 col-md-6">
+                    <p>Maecenas sodales ligula non velit pretium, ac aliquet nisi auctor. Morbi eu ipsum sit amet nunc sodales volutpat. Vivamus dignissim interdum ex quis tincidunt. Sed vitae felis tellus. Mauris tristique nisl ut quam faucibus, ac volutpat urna dictum. Vestibulum tincidunt neque molestie ante mollis scelerisque. Nullam condimentum sodales tellus. Sed convallis magna nec tincidunt malesuada.</p>
+                    <p>Quisque auctor sapien sed nisi bibendum, in porta risus scelerisque. Phasellus sagittis, velit quis euismod elementum, eros nisl bibendum turpis, et volutpat velit orci non lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi nec elit ex.</p>
+                </div>
+                <div class="class-xs-12 col-sm-6 col-md-6">
+                    <p>In at felis pretium erat pellentesque pulvinar. Nam lobortis neque eu laoreet rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et condimentum urna, vitae tempor ante. Etiam id lorem dui. Maecenas a lacinia lacus. Integer congue eget dolor sit amet scelerisque.</p>
+                    <p>Duis leo neque, viverra vel erat tempor, laoreet dictum elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam id efficitur odio. Vivamus eleifend odio elit, sit amet lobortis nunc ornare eu. Nulla viverra nisl nec massa porttitor commodo. Suspendisse venenatis risus id ante consequat, at feugiat ex rutrum. Nam commodo rutrum congue.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="about-bottom">
+            <span>“</span>
+            <p>Maecenas sodales ligula non velit pretium, ac aliquet nisi auctor. Morbi eu ipsum sit amet nunc sodales volutpat. Vivamus dignissim interdum ex quis tincidunt. Sed vitae felis tellus. Mauris tristique nisl ut quam faucibus, ac volutpat urna dictum. Vestibulum tincidunt neque molestie ante mollis scelerisque. Nullam condimentum sodales tellus. Sed convallis magna nec tincidunt malesuada. Quisque auctor sapien sed nisi bibendum, in porta risus scelerisque.</p>
+            <h5>John Smith</h5>
+            <span class="speciality">designer</span>
+        </div>
+        <button class="main-btn" type="submit">join us</button>
+    </div>
+</section>
+<!-- ABOUT -->

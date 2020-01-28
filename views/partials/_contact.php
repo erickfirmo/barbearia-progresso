aa6cdf315ff609708eeccb672a4a40167a5bf45b
Erick <erickluques@gmail.com>
2020-01-28 02:03:52 -0200

    Create _contact.php

---
+++ views/partials/_contact.php
@@ -0,0 +1,44 @@
+
+	<!-- CONTACT -->
+	<section id="contact" class="contact">
+		<div class="container">
+			<div class="h-page-title">
+				<h1>contacts</h1>
+				<span class="sub-title">salon location</span>
+				<div class="mustache">
+					<span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+				</div>
+			</div>
+		</div>
+
+		<!-- Google Map Start -->
+		<div class="map">
+			<div id="map">
+			</div>
+			<div class="map-box">
+				<div class="container">
+					<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 map-detail">
+						<ul>
+							<li>
+								<img class="img-responsive" src="assets/images/home-page/contact-img1.png" alt="">
+								<h4>Address</h4>
+								<p>3267 Willis Avenue Hastings, FL 32145</p>
+							</li>
+							<li>
+								<img class="img-responsive" src="assets/images/home-page/contact-img2.png" alt="">
+								<h4>Phone</h4>
+								<p>+1 959 603 6035</p>
+							</li>
+							<li>
+								<img class="img-responsive" src="assets/images/home-page/contact-img3.png" alt="">
+								<h4>Email</h4>
+								<a href="#">up@sitename.com</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Google Map End -->
+	</section>
+	<!-- CONTACT -->

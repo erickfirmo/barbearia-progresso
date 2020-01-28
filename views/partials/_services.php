c86dd6ad1131aa78105f862554c4cc4450ad7ffe
Erick <erickluques@gmail.com>
2020-01-28 02:05:04 -0200

    Create _services.php

---
+++ views/partials/_services.php
@@ -0,0 +1,63 @@
+<!-- SERVICES -->
+<section id="service" class="service">
+    <div class="container">
+        <div class="h-page-title">
+            <h1>services</h1>
+            <span class="sub-title">what we can do</span>
+            <div class="mustache">
+                <span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+            </div>
+        </div>
+        <div class="main-service">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 service-left">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img1.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img1.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>traditional haircuts</h4>
+                        <p>Vivamus dignissim interdum ex quis tincidunt. Sed vitae felis tellus. Mauris tristique nisl ut quam</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 service-right">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img2.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img2.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>manicure</h4>
+                        <p>Aenean eu nisl ut neque commodo volutpat at eu magna. Nulla at bibendum eros.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 service-left">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img3.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img3.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>head shaves</h4>
+                        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 service-right">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img4.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img4.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>stubble</h4>
+                        <p>Nullam condimentum sodales tellus. Sed convallis magna nec tincidunt malesuada.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 service-left">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img5.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img5.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>Shaves</h4>
+                        <p>Integer ultricies magna ac elementum condimentum. Aliquam molestie rhoncus</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 service-right">
+                    <span class="ico"><img class="icon_1" src="assets/images/home-page/service-img6.png" alt=""><img class="icon_2" src="assets/images/home-page/hover-service-img6.png" alt=""></span>
+                    <div class="service-info">
+                        <h4>restyle</h4>
+                        <p>Mauris tristique nisl ut quam faucibus, ac volutpat urna dictum. Vestibulum tincidunt</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- SERVICES -->

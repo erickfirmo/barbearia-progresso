8732468982f80e94fefdd87d9577dc6684e17be3
Erick <erickluques@gmail.com>
2020-01-28 00:21:58 -0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,12 +6,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Creating the router instance
 $router = new \ErickFirmo\Router;
 
-// Defining namespaces
-$router->setNamespace('System\\');
-
 // Defining routes
 require_once __DIR__ . '/../src/routes.php';
-
 
 // Run the router
 $router->run();

35e8f441f2e97cc0e6eb44177a2dfd5cf6a8bf97
Erick <erickluques@gmail.com>
2020-01-28 02:03:41 -0200

    Create _blog.php

---
+++ views/partials/_blog.php
@@ -0,0 +1,114 @@
+<!-- BLOG
+<section id="blog" class="blog">
+    <div class="container">
+        <div class="h-page-title">
+            <h1>blog</h1>
+            <span class="sub-title">recent posts</span>
+            <div class="mustache">
+                <span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+            </div>
+        </div>
+        <div class="main-blog">
+            <ul class="masonry-feed row">
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/1.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">Pellentesque eleifend erat vitae leo faucibus, non commodo est accumsan. Morbi</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/2.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">faucibus, non commodo est accumsan.</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/3.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">Donec a ipsum pharetra pretium neque</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/4.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">pretium neque nec, pharetra lorem. Aenean et</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/5.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">Aenean et luctus eros, at blandit diam. Cum sociis</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="masonry-item">
+                    <div class="box-holder">
+                        <a href="./blog-single.html">
+                            <div class="overlay-bg"></div>
+                            <img src="assets/images/home-page/blog/6.jpg" alt="">
+                        </a>
+                        <div class="blog-detail">
+                            <div class="link">
+                                <a href="./blog-single.html">Aenean et luctus eros, at blandit diam. Cum sociis</a>
+                            </div>
+                            <div class="date">
+                                <h6>January 24, 2015</h6>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <button class="main-btn" type="submit">more</button>	
+        </div>
+    </div>
+</section>
+<!-- BLOG -->

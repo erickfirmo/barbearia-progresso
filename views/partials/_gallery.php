d3545f1816a146e8bc070f14d21bc63e63194ab7
Erick <erickluques@gmail.com>
2020-01-28 02:04:11 -0200

    Create _gallery.php

---
+++ views/partials/_gallery.php
@@ -0,0 +1,87 @@
+<!-- GALLERY -->
+<section id="gallery" class="gallery">
+		<div class="container">
+			<div class="h-page-title">
+				<h1>gallery</h1>
+				<span class="sub-title">our works</span>
+				<div class="mustache">
+					<span><img class="img-responsive" src="assets/images/home-page/mustache.png" alt=""></span>
+				</div>
+			</div>
+		</div>
+		<div id="portfolio-gal">
+			<div class="col-md-12 main-navigation">
+				<ul id="filters" class="clearfix portfolio-gal-filter" data-option-key="filter">
+					<li><a href="#filter" class="active" data-option-value="*">All</a></li>
+					<li><a href="#filter" data-option-value=".headshaves">Head shaves</a></li>
+					<li><a href="#filter" data-option-value=".restyle">Restyle</a></li>
+					<li><a href="#filter" data-option-value=".manicure">Manicure</a></li>
+					<li><a href="#filter" data-option-value=".stubble">Stubble</a></li>
+				</ul>
+			</div>
+			<div class="col-md-12">
+				<div class="portfolio-gal isotope">
+					<div class="folio-item headshaves col-sm-12 col-sm-6 col-md-6 col-lg-6">
+						<div class="folio-thumb">
+							<a href="images/portfolio/1/1.jpg" class="pop_lbox">
+								<div class="overlay-bg"></div>
+								<img src="assets/images/home-page/projects/1.jpg" class="img-responsive" alt=""/>
+							</a>
+							<div class="folio-border">
+								<div class="folio-detail">
+									<h5>OUR Best shaves</h5>
+									<h6>design and idea</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="folio-item restyle col-sm-12 col-sm-6 col-md-6 col-lg-6">
+						<div class="folio-thumb">
+							<a href="images/portfolio/1/2.jpg" class="pop_lbox">
+								<div class="overlay-bg"></div>
+								<img src="assets/images/home-page/projects/2.jpg" class="img-responsive" alt=""/>
+							</a>
+							<div class="folio-border">
+								<div class="folio-detail">
+									<h5>OUR Best restle</h5>
+									<h6>design and idea</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="folio-item manicure col-sm-12 col-sm-6 col-md-6 col-lg-6">
+						<div class="folio-thumb">
+							<a href="images/portfolio/1/3.jpg" class="pop_lbox">
+								<div class="overlay-bg"></div>
+								<img src="assets/images/home-page/projects/3.jpg" class="img-responsive" alt=""/>
+							</a>
+							<div class="folio-border">
+								<div class="folio-detail">
+									<h5>OUR Best manicure</h5>
+									<h6>design and idea</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="folio-item stubble col-sm-12 col-sm-6 col-md-6 col-lg-6">
+						<div class="folio-thumb">
+							<a href="images/portfolio/1/4.jpg" class="pop_lbox">
+								<div class="overlay-bg"></div>
+								<img src="assets/images/home-page/projects/4.jpg" class="img-responsive" alt=""/>
+							</a>
+							<div class="folio-border">
+								<div class="folio-detail">
+									<h5>OUR Best stubble</h5>
+									<h6>design and idea</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<div class="space30"></div>
+				<a href="./blog.html" class="main-btn">more</a>
+			</div>
+		</div>
+	</section>
+	<!-- GALLERY -->

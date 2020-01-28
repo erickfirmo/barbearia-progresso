164294139bea9d2b275d8cf0c542b25fdb21577e
Erick <erickluques@gmail.com>
2020-01-28 02:04:19 -0200

    Create _header.php

---
+++ views/partials/_header.php
@@ -0,0 +1,97 @@
+<!-- INTRO -->
+<div id="intro">
+    <header class="header">
+        <div class="stick">
+            <div id="main-nav">
+                <nav class="navbar navbar-inverse one-menu-top hidden-xs hidden-sm">
+                    <div class="container-fluid">
+                        <div class="navbar-collapse collapse" id="navbar">
+                            <div class="col-md-5">
+                                <div class="pull-right onemenu-right">
+                                <ul class="nav navbar-nav">
+                                    <!-- <li class="page-scroll"><a href="#about">About</a></li> -->
+                                    <li class="page-scroll"><a href="#service">Serviços</a></li>
+                                    <li class="page-scroll"><a href="#gallery">Galeria</a></li>
+                                </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-md-2">
+                                <a href="./index.html" class="navbar-brand"><img class="img-responsive" src="assets/images/home-page/main-page-logo.png" alt=""></a>
+                            </div>
+
+                            <div class="col-md-5">
+                                <div class="pull-left onemenu-left">
+                                <ul class="nav navbar-nav">
+                                    <li class="page-scroll"><a href="#pricing">Preço</a></li>
+                                    <!-- <li class="page-scroll"><a href="#blog">Blog</a></li> -->
+                                    <li class="page-scroll"><a href="#contact">Contato</a></li>
+                                </ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </nav>
+
+                <nav class="navbar navbar-inverse one-menu-top-ipad visible-sm hidden-xs hidden-md hidden-lg">
+                    <div class="container-fluid">
+                        <div class="navbar-collapse collapse" id="navbar2">
+                            <div class="col-md-12">
+                                <a href="./index.html" class="navbar-brand"><img class="img-responsive" src="assets/images/home-page/main-page-logo.png" alt=""></a>
+
+                                <ul class="nav navbar-nav">
+                                    <!-- <li class="page-scroll"><a href="#about">About</a></li> -->
+                                    <li class="page-scroll"><a href="#service">Serviços</a></li>
+                                    <li class="page-scroll"><a href="#gallery">Galeria</a></li>
+                                    <li class="page-scroll"><a href="#pricing">Preço</a></li>
+                                    <!-- <li class="page-scroll"><a href="#blog">Blog</a></li> -->
+                                    <li class="page-scroll"><a href="#contact">Contato</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <nav class="navbar navbar-inverse one-menu-top-mob visible-xs hidden-sm hidden-md hidden-lg">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a href="./index.html" class="navbar-brand"><img class="img-responsive" src="assets/images/home-page/main-page-logo.png" alt=""></a>
+                            <br>
+                            <button type="button" class="mob-menu-trigger navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                                <span>Menu</span>
+                                <span class="fa fa-bars"></span>
+                            </button>
+                        </div>
+                        <div class="mob-menu" id="navbar3">
+                            <ul>
+                                <!-- <li class="page-scroll"><a href="#about">About</a></li> -->
+                                <li class="page-scroll"><a href="#service">Serviços</a></li>
+                                <!-- <li class="page-scroll"><a href="#team-barber">The barbers</a></li> -->
+                                <li class="page-scroll"><a href="#gallery">Galeria</a></li>
+                                <li class="page-scroll"><a href="#pricing">Preço</a></li>
+                                <!-- <li class="page-scroll"><a href="#blog">Blog</a></li> -->
+                                <li class="page-scroll"><a href="#contact">Contato</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+        <div class="flexslider">
+            <ul class="slides">
+                <li>
+                    <img src="assets/images/slider/1/1.jpg" alt=""/>
+                </li>
+                <li>
+                    <img src="assets/images/slider/1/2.jpg" alt=""/>
+                </li>
+                <li>
+                    <img src="assets/images/slider/1/3.jpg" alt=""/>
+                </li>
+            </ul>
+        </div>
+    </header>
+</div>
+<!-- INTRO -->

4ba262dc01186a7f9eeab00d4a994f6b8de5f868
Erick <erickluques@gmail.com>
2020-01-28 02:03:59 -0200

    Create _footer.php

---
+++ views/partials/_footer.php
@@ -0,0 +1,20 @@
+<!-- FOOTER -->
+<footer id="footer" class="blog-f">
+    <div class="container">
+        <span>Follow, Like, Pin all the social stuff</span>
+        <ul class="footer-social">
+            <li><a href="#" class="fa fa-twitter"></a></li>
+            <li><a href="#" class="fa fa-facebook"></a></li>
+            <li><a href="#" class="fa fa-google-plus"></a></li>
+            <li><a href="#" class="fa fa-dribbble"></a></li>
+            <li><a href="#" class="fa fa-linkedin"></a></li>
+            <li><a href="#" class="fa fa-pinterest"></a></li>
+            <li><a href="#" class="fa fa-flickr"></a></li>
+            <li><a href="#" class="fa fa-youtube"></a></li>
+            <li><a href="#" class="fa fa-rss"></a></li>
+        </ul>
+        <p>Barbershop &copy;Copyright 2015</p>
+        <p>All rights reserved <a target="_blank" href="http://themeforest.net/user/Theme-Squared">ThemeSquared</a></p>
+    </div>
+</footer>
+<!-- FOOTER -->

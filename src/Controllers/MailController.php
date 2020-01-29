9ffb4779ea03e69b1f9d26e28db0aac98b409891
Erick <erickluques@gmail.com>
2020-01-29 17:48:38 -0200

    Update MailController.php

--- src/Controllers/MailController.php
+++ src/Controllers/MailController.php
@@ -7,11 +7,32 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 class MailController extends Controller {
+
+    public $required_fields = [
+
+    ];
+
     public function __construct(){
 
+        
     }
 
-    public function send(){
+    public function send() {
+
+        if(isset($_POST['form_send_to']) && !empty($_POST['form_send_to'])) {
+
+            $_POST['form_send_to'] = base64_decode($_POST['form_send_to']);
+
+            if(strpos($_POST['form_send_to'], ",") !== false)
+                $email_to = explode(",", $_POST['form_send_to']);
+            else if(strpos($_POST['form_send_to'], ";") !== false)
+                $email_to = explode(";", $_POST['form_send_to']);
+            else
+                $email_to = $_POST['form_send_to'];
+
+        } else {
+            $email_to = 'tests@erickfirmo.dev';
+        }
 
     }
 }

f340923a29736ad026659c796ea3544a3b5faa11
erickfirmo <erickluque@gmail.com>
2020-02-15 13:00:36 -0300

    Update index.php

--- views/index.php
+++ views/index.php
@@ -100,7 +100,7 @@
 
 <!-- Google reCaptcha site_key -->
 <script>
-	recaptcha_site_key = '<site_key>';
+	var recaptcha_site_key = '<site_key>';
 	for (var i = 0; i < $('.g-recaptcha').length; i++) {
 		$('.g-recaptcha').eq(i).attr('data-sitekey', recaptcha_site_key);
 	}

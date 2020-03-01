a614da48ded9f4e5ad1c3cda39d2cfdd2b91e7a0
Érick Firmo <erickluques@gmail.com>
2020-02-29 23:47:39 -0300

    Create smtp config file

---
+++ src/config/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'SMTP_DEBUG' => 1,
+    'SMTP_HOST' => '',
+    'SMTP_PORT' => '',
+    'SMTP_SECURE' => 'ssl',
+    'SMTP_AUTH' => true,
+    'SMTP_USER' => '',
+    'SMTP_PASS' => '',
+];

a5ad0d1491f2bb6109a5c71be2d9743e69d5e435
Érick Firmo <erickluques@gmail.com>
2020-03-02 01:46:29 -0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,16 @@
 <?php
 
+$app = include __DIR__.'/../config/app.php';
+
+if (!function_exists('app'))
+{
+	function app($key)
+	{
+		if(isset($key))
+			return array_key_exists($key, $app) ? $app[$key] : null;
+	}
+}
+
 // functions
 
 if (!function_exists('view'))
@@ -22,15 +33,15 @@ if (!function_exists('redirect'))
 {
 	function redirect($route)
 	{
-    	//header('Location: '.(isset($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].$route);
-	    //exit();
+		header('Location: '.(isset($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].$route);
+	    exit;
 	}
 }
 
 if (!function_exists('asset'))
 {
-    function asset($path, $root = '/projetos/barbearia-progresso/public/') 
+    function asset($path) 
     {
-        echo $root.'assets/'.$path;
+        echo $app['base_url'].'assets/'.$path;
     }
 }

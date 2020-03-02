901c761dc8bcb079cbae13ef6175a9643f92afe6
Érick Firmo <erickluques@gmail.com>
2020-03-02 03:25:01 -0300

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -4,10 +4,17 @@ for (var i = 0; i < $('.g-recaptcha').length; i++) {
     $('.g-recaptcha').eq(i).attr('data-sitekey', recaptcha_site_key);
 }
 
+function scrollTopAnimate(selector, time) {
+    $(function() {
+        $('html, body').animate({
+            scrollTop: $(selector).offset().top
+        }, time);
+    });
+}
+
 /* scroll to form on click in price box */
 $('.inner-price').on('click', function() {
     $('#senderService option[value="'+$(this).find('button').attr('data-service-type')+'"]').prop('selected', true);
-        $('html, body').animate({
-            scrollTop: $("#contact").offset().top
-        }, 1500);
+        scrollTopAnimate('#contact', 1500);
 });
+

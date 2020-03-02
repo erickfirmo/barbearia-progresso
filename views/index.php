cc6e5cc1ad5eeeb9735f99ddea96f6790948d487
Érick Firmo <erickluques@gmail.com>
2020-03-02 02:43:12 -0300

    Update index.php

--- views/index.php
+++ views/index.php
@@ -106,18 +106,6 @@
 
 <script>
 
-    /* Google reCaptcha site_key --> */
-	var recaptcha_site_key = '<site_key>';
-	for (var i = 0; i < $('.g-recaptcha').length; i++) {
-		$('.g-recaptcha').eq(i).attr('data-sitekey', recaptcha_site_key);
-	}
-
-	$('.inner-price').on('click', function() {
-		$('#senderService option[value="'+$(this).find('button').attr('data-service-type')+'"]').prop('selected', true);
-			$('html, body').animate({
-				scrollTop: $("#contact").offset().top
-			}, 1500);
-	});
 </script>
 
 </body>

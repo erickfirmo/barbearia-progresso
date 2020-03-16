d65ae61db007fed4363419d39daa8dd11c621066
Érick Firmo <erickluques@gmail.com>
2020-03-16 01:43:26 -0300

    Update _form.php

--- views/partials/_form.php
+++ views/partials/_form.php
@@ -2,15 +2,15 @@
 
 	<form id="contactForm" action="agendar" method="post" class="positioned">
 		<div class="row">
-			<div class="col-md-6">
-				<div class="row control-group">
-					<div class="form-group col-xs-12 controls">
+			<div class="col-md-12">
+				<div class="control-group">
+					<div class="form-group col-xs-6 controls">
 						<input class="form-control" name="name" id="senderName" type="text" placeholder="Nome" required="required">                       
 						<p class="help-block"></p>
 					</div>
 				</div>
-				<div class="row control-group">
-					<div class="form-group col-xs-12 controls">
+				<div class="control-group">
+					<div class="form-group col-xs-6 controls">
 						<select class="form-control custom-select" name="service" id="senderService" placeholder="Serviço" required="required">
 							<option selected>--</option>
 							<option value="Corte Masculino">Corte Masculino</option>
@@ -23,21 +23,21 @@
 						<p class="help-block"></p>
 					</div>
 				</div>
-				<div class="row control-group">
-					<div class="form-group col-xs-12  controls">
+				<div class="control-group">
+					<div class="form-group col-xs-6  controls">
 						<input class="form-control" placeholder="Telefone ou Celular" name="phone" id="senderPhone" type="text">
 						<p class="help-block"></p>
 					</div>
 				</div>
-				<div class="row control-group">
-					<div class="form-group col-xs-12 controls">
+				<div class="control-group">
+					<div class="form-group col-xs-6 controls">
 						<input class="form-control" type="email" name="email" id="senderEmail" placeholder="Email" required="required">
 						<p class="help-block"></p>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-6">
-				<div class="row control-group">
+			<div class="col-md-12">
+				<div class="control-group">
 					<div class="form-group col-xs-12 controls">
 						<textarea rows="7" class="form-control" name="message" id="message" placeholder="Mensagem" style="height:275px;"></textarea>
 						<p class="help-block"></p>

2170b74a31182d0444c5dfba5fdcacb521dfcfd2
Érick Firmo <erickluques@gmail.com>
2020-03-19 02:41:13 -0300

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -25,3 +25,42 @@ $('.inner-price').on('click', function() {
         scrollTopAnimate('#contact', 1500);
 });
 
+
+/* Lightbox gallery */
+
+function openModal() {
+  document.getElementById("lightbox-model").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("lightbox-model").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("lightbox-slides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
